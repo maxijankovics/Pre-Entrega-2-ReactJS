4ddb3a431b9427dcd8a6e75755292a46e012bb89
Maximiliano Jankovics <maxijankovics@gmail.com>
2024-09-28 19:09:16 -0300

    primer commit

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+
+import obtenerProductos from '../data/productos';
+import { useParams } from "react-router-dom";
+
+function ItemListContainer() {
+  return (
+    <div>ItemListContainer</div>
+  )
+}
+
+export default ItemListContainer
---
+++ src/data/productos.js
@@ -0,0 +1,23 @@
+const productos = [
+    {id: 1, nombre: "Menu ejecutivo", categoria: "menus completos", precio: 540, stock: 40, rutaImagen: "../assets/menu-ejecutivo.png"},
+    {id: 2, nombre: "Menu express", categoria: "menus completos", precio: 370, stock: 50, rutaImagen: "../assets/menu-express.jpg"},
+    {id: 3, nombre: "Petit entrecot con guarnicion", categoria: "platos carta", precio: 545, stock: 10, rutaImagen: "../assets/Entrecot-con-guarnición.jpg"},
+    {id: 4, nombre: "Costillas de cerdo agridulces (con ananá) con guarnicion", categoria: "platos carta", precio: 520, stock: 10, rutaImagen: "../assets/costillas-de-cerdo-agridulces.jpg"},
+    {id: 5, nombre: "Sorrentinos caseros", categoria: "platos carta", precio: 410, stock: 10, rutaImagen: "../assets/sorrentinos-caseros.jpg"},
+    {id: 6, nombre: "Canelones de verdura con fileto", categoria: "platos carta", precio: 410, stock: 10, rutaImagen: "../assets/canelones-de-verdura.jpg"},
+    {id: 7, nombre: "Berenjenas a la parmesana", categoria: "platos carta", precio: 450, stock: 10, rutaImagen: "../assets/berenjenas-a-la-parmesana.jpg"},
+    {id: 8, nombre: "Copa de vino tinto", categoria: "vinos", precio: 150, stock: 60, rutaImagen: "../assets/copa-de-vino-tinto.png"},
+    {id: 9, nombre: "Copa de vino blanco", categoria: "vinos", precio: 150, stock: 60, rutaImagen: "../assets/copa-vino-blanco.jpg"},
+    {id: 10, nombre: "Pepsi", categoria: "refrescos", precio: 120, stock: 100, rutaImagen: "../assets/pepsi.png"},
+    {id: 11, nombre: "Pepsi black", categoria: "refrescos", precio: 120, stock: 100, rutaImagen: "../assets/pepsi-black.png"},
+    {id: 12, nombre: "Mirinda", categoria: "refrescos", precio: 120, stock: 100, rutaImagen: "../assets/mirinda.png"},
+    {id: 13, nombre: "7up", categoria: "refrescos", precio: 120, stock: 100, rutaImagen: "../assets/7up.png"},
+]
+
+const obtenerProductos = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(productos);
+    }, 3000);
+});
+
+export default obtenerProductos
